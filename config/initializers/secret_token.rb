208a69224250899d50bd7d4a7a7f0f606283ceb8
James McKinney <james@slashpoundbang.com>
2011-12-07 00:01:08 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatinerMontreal::Application.config.secret_token = '2a2277cec90e56e87c80f7e48fa8c95162ee31f11e572a2e7256c7754b30cea8a387a511f48e3e9558833ab73fe6d18cdfb8f6087456bf8bbd2a392de1ea6459'
