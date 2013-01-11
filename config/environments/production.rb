bc2f82dd7320b72bcad388e82f437cc8f8a1efe2
James McKinney <james@slashpoundbang.com>
2013-01-10 23:44:22 -0500

    add to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ PatinerMontreal::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(leaflet.ie.css modernizr-2.0.6.min.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
