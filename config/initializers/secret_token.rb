d0be29e35085370ad994a39274b548f12ccc4fe9
James McKinney <james@slashpoundbang.com>
2013-01-18 22:29:10 -0500

    remove unnecessary files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PatinerMontreal::Application.config.secret_token = '2a2277cec90e56e87c80f7e48fa8c95162ee31f11e572a2e7256c7754b30cea8a387a511f48e3e9558833ab73fe6d18cdfb8f6087456bf8bbd2a392de1ea6459'
+PatinerMontreal::Application.config.secret_token = ENV['SECRET_TOKEN'] || '2a2277cec90e56e87c80f7e48fa8c95162ee31f11e572a2e7256c7754b30cea8a387a511f48e3e9558833ab73fe6d18cdfb8f6087456bf8bbd2a392de1ea6459'
