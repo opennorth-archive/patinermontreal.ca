c2346d924acd46c65a752a013e51eec38a862803
mudar <mn@mudar.ca>
2021-03-30 10:20:53 -0400

    https updates for stamen map tiles

--- js/stamen.js
+++ js/stamen.js
@@ -7,52 +7,63 @@
 var SUBDOMAINS = "a. b. c. d.".split(" "),
     MAKE_PROVIDER = function(layer, type, minZoom, maxZoom) {
         return {
-            "url":          ["http://{S}tile.stamen.com/", layer, "/{Z}/{X}/{Y}.", type].join(""),
+            "url":          ["//stamen-tiles-{S}a.ssl.fastly.net/", layer, "/{Z}/{X}/{Y}.", type].join(""),
             "type":         type,
             "subdomains":   SUBDOMAINS.slice(),
             "minZoom":      minZoom,
             "maxZoom":      maxZoom,
             "attribution":  [
-                'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ',
+                'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
                 'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
-                'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
+                'Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, ',
                 'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
             ].join("")
         };
     },
     PROVIDERS =  {
         "toner":        MAKE_PROVIDER("toner", "png", 0, 20),
-        "terrain":      MAKE_PROVIDER("terrain", "jpg", 4, 18),
-        "watercolor":   MAKE_PROVIDER("watercolor", "jpg", 1, 18),
-        "trees-cabs-crime": {
-            "url": "http://{S}.tiles.mapbox.com/v3/stamen.trees-cabs-crime/{Z}/{X}/{Y}.png",
-            "type": "png",
-            "subdomains": "a b c d".split(" "),
-            "minZoom": 11,
-            "maxZoom": 18,
-            "extent": [
-                {"lat": 37.853, "lon": -122.577},
-                {"lat": 37.684, "lon": -122.313}
-            ],
-            "attribution": [
-                'Design by Shawn Allen at <a href="http://stamen.com">Stamen</a>.',
-                'Data courtesy of <a href="http://fuf.net">FuF</a>,',
-                '<a href="http://www.yellowcabsf.com">Yellow Cab</a>',
-                '&amp; <a href="http://sf-police.org">SFPD</a>.'
-            ].join(" ")
-        }
+        "terrain":      MAKE_PROVIDER("terrain", "png", 0, 18),
+        "terrain-classic": MAKE_PROVIDER("terrain-classic", "png", 0, 18),
+        "watercolor":   MAKE_PROVIDER("watercolor", "jpg", 1, 18)
     };
+
+PROVIDERS["terrain-classic"].url = "//stamen-tiles-{S}a.ssl.fastly.net/terrain/{Z}/{X}/{Y}.png";
 
 // set up toner and terrain flavors
 setupFlavors("toner", ["hybrid", "labels", "lines", "background", "lite"]);
-setupFlavors("terrain", ["background"]);
-setupFlavors("terrain", ["labels", "lines"], "png");
+setupFlavors("terrain", ["background", "labels", "lines"]);
 
 // toner 2010
 deprecate("toner", ["2010"]);
 
 // toner 2011 flavors
 deprecate("toner", ["2011", "2011-lines", "2011-labels", "2011-lite"]);
+
+var odbl = [
+    "toner",
+    "toner-hybrid",
+    "toner-labels",
+    "toner-lines",
+    "toner-background",
+    "toner-lite",
+    "terrain",
+    "terrain-background",
+    "terrain-lines",
+    "terrain-labels",
+    "terrain-classic"
+];
+
+for (var i = 0; i < odbl.length; i++) {
+    var key = odbl[i];
+
+    PROVIDERS[key].retina = true;
+    PROVIDERS[key].attribution = [
+        'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
+        'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
+        'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    ].join("");
+}
 
 /*
  * Export stamen.tile to the provided namespace.
@@ -155,14 +166,15 @@ if (typeof L === "object") {
                     "maxZoom":      provider.maxZoom,
                     "subdomains":   provider.subdomains,
                     "scheme":       "xyz",
-                    "attribution":  provider.attribution
+                    "attribution":  provider.attribution,
+                    sa_id:          name
                 });
             L.TileLayer.prototype.initialize.call(this, url, opts);
         }
     });
 
     /*
-     * Factory function for consistency with Leaflet conventions 
+     * Factory function for consistency with Leaflet conventions
      */
     L.stamenTileLayer = function (options, source) {
         return new L.StamenTileLayer(options, source);
@@ -218,6 +230,21 @@ if (typeof OpenLayers === "object") {
  * <https://developers.google.com/maps/documentation/javascript/>
  */
 if (typeof google === "object" && typeof google.maps === "object") {
+
+    // Extending Google class based on a post by Bogart Salzberg of Portland Webworks,
+    // http://www.portlandwebworks.com/blog/extending-googlemapsmap-object
+    google.maps.ImageMapType = (function(_constructor){
+        var f = function() {
+            if (!arguments.length) {
+                return;
+            }
+            _constructor.apply(this, arguments);
+        }
+        f.prototype = _constructor.prototype;
+        return f;
+    })(google.maps.ImageMapType);
+
+
     google.maps.StamenMapType = function(name) {
         var provider = getProvider(name),
             subdomains = provider.subdomains;
@@ -240,8 +267,10 @@ if (typeof google === "object" && typeof google.maps === "object") {
             "maxZoom":  provider.maxZoom
         });
     };
+
     // FIXME: is there a better way to extend classes in Google land?
-    google.maps.StamenMapType.prototype = new google.maps.ImageMapType("_");
+    // Possibly fixed, see above ^^^ | SC
+    google.maps.StamenMapType.prototype = new google.maps.ImageMapType;
 }
 
 })(typeof exports === "undefined" ? this : exports);
